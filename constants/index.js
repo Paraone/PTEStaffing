1a6d896edbd617b32c13476d87310b398479e377
Marcos Wade <marcoswade@gmail.com>
2022-11-24 16:01:42 -0400

    move user functions to userController.js

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+export const WORKER_ACCOUNT_TYPE = 'worker';
+export const CLIENT_ACCOUNT_TYPE = 'client';
+export const ADMIN_ACCOUNT_TYPE = 'admin';
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,35 @@
-// import { useEffect } from 'react';
-// import axios from 'axios';
+const bcrypt = require('bcrypt');
+const v4 = require('uuid').v4;
+import { WORKER_ACCOUNT_TYPE } from '~constants';
 
-// const getUsers = (callback) => {
-//      callback();
-// };
+const saltRounds = 10;
+
+export async function findWorker(db, email, profileId) {
+    const collection = db.collection('user');
+    return await collection.findOne({ $or: [{ email }, { profileId }]}, { projection: { _id: 0, password: 0 }});
+}
+
+export async function createWorker(db, firstName, lastName, email, password = 'w', profileId) {
+    const collection = db.collection('user');
+    console.log('create workder')
+    return await bcrypt.hash(password, saltRounds).then(async (hash) => {
+        console.log({ hash })
+        if (!hash) {
+            return { error: 'No hash returned.'};
+        }
+        const confirmationCode = v4();
+        return await collection.insertOne(
+            {
+                userId: v4(),
+                firstName,
+                lastName,
+                email,
+                profileId,
+                confirmationCode,
+                accountType: WORKER_ACCOUNT_TYPE,
+                emailConfirmed: false,
+                password: hash,
+            }
+        );
+    })
+}
